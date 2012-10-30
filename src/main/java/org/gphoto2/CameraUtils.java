49ad2a2bb6029e765492904542d4fc9bb76eb8e8
Martin Vysny <martin.vysny@innovatrics.com>
2012-10-30 10:25:31 +0100

    Added more error codes

--- src/main/java/org/gphoto2/CameraUtils.java
+++ src/main/java/org/gphoto2/CameraUtils.java
@@ -20,6 +20,8 @@ package org.gphoto2;
 
 import java.io.Closeable;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.gphoto2.jna.GPhoto2Native;
@@ -43,8 +45,12 @@ public class CameraUtils {
     private final static Logger log = Logger.getLogger(CameraUtils.class.getName());
 
     public static int check(int result, String methodName) {
-	if (result < 0) {
-	    throw new GPhotoException(methodName + " failed with #" + result + ": " + GPhoto2Native.INSTANCE.gp_result_as_string(result), result);
+	if (result != GPhoto2Native.GP_OK) {
+        String constantName = ERROR_CONSTANTS.get(result);
+        if (constantName == null) {
+            constantName ="unknown error";
+        }
+	    throw new GPhotoException(methodName + " failed with " + constantName + " #" + result + ": " + GPhoto2Native.INSTANCE.gp_result_as_string(result), result);
 	}
 	return result;
     }
@@ -76,5 +82,43 @@ public class CameraUtils {
         } catch (UnsupportedEncodingException ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    private static final Map<Integer, String> ERROR_CONSTANTS = new HashMap<Integer, String>();
+    static {
+        final Map<Integer, String> m = ERROR_CONSTANTS;
+        m.put(GPhoto2Native.GP_ERROR_CORRUPTED_DATA, "GP_ERROR_CORRUPTED_DATA");
+        m.put(GPhoto2Native.GP_ERROR_FILE_EXISTS, "GP_ERROR_FILE_EXISTS");
+        m.put(GPhoto2Native.GP_ERROR_MODEL_NOT_FOUND, "GP_ERROR_MODEL_NOT_FOUND");
+        m.put(GPhoto2Native.GP_ERROR_DIRECTORY_NOT_FOUND, "GP_ERROR_DIRECTORY_NOT_FOUND");
+        m.put(GPhoto2Native.GP_ERROR_FILE_NOT_FOUND, "GP_ERROR_FILE_NOT_FOUND");
+        m.put(GPhoto2Native.GP_ERROR_DIRECTORY_EXISTS, "GP_ERROR_DIRECTORY_EXISTS");
+        m.put(GPhoto2Native.GP_ERROR_CAMERA_BUSY, "GP_ERROR_CAMERA_BUSY");
+        m.put(GPhoto2Native.GP_ERROR_PATH_NOT_ABSOLUTE, "GP_ERROR_PATH_NOT_ABSOLUTE");
+        m.put(GPhoto2Native.GP_ERROR_CANCEL, "GP_ERROR_CANCEL");
+        m.put(GPhoto2Native.GP_ERROR_CAMERA_ERROR, "GP_ERROR_CAMERA_ERROR");
+        m.put(GPhoto2Native.GP_ERROR_OS_FAILURE, "GP_ERROR_OS_FAILURE");
+        m.put(GPhoto2Native.GP_OK, "GP_OK");
+        m.put(GPhoto2Native.GP_ERROR, "GP_ERROR");
+        m.put(GPhoto2Native.GP_ERROR_BAD_PARAMETERS, "GP_ERROR_BAD_PARAMETERS");
+        m.put(GPhoto2Native.GP_ERROR_NO_MEMORY, "GP_ERROR_NO_MEMORY");
+        m.put(GPhoto2Native.GP_ERROR_LIBRARY, "GP_ERROR_LIBRARY");
+        m.put(GPhoto2Native.GP_ERROR_UNKNOWN_PORT, "GP_ERROR_UNKNOWN_PORT");
+        m.put(GPhoto2Native.GP_ERROR_NOT_SUPPORTED, "GP_ERROR_NOT_SUPPORTED");
+        m.put(GPhoto2Native.GP_ERROR_IO, "GP_ERROR_IO");
+        m.put(GPhoto2Native.GP_ERROR_FIXED_LIMIT_EXCEEDED, "GP_ERROR_FIXED_LIMIT_EXCEEDED");
+        m.put(GPhoto2Native.GP_ERROR_TIMEOUT, "GP_ERROR_TIMEOUT");
+        m.put(GPhoto2Native.GP_ERROR_IO_SUPPORTED_SERIAL, "GP_ERROR_IO_SUPPORTED_SERIAL");
+        m.put(GPhoto2Native.GP_ERROR_IO_SUPPORTED_USB, "GP_ERROR_IO_SUPPORTED_USB");
+        m.put(GPhoto2Native.GP_ERROR_IO_INIT, "GP_ERROR_IO_INIT");
+        m.put(GPhoto2Native.GP_ERROR_IO_READ, "GP_ERROR_IO_READ");
+        m.put(GPhoto2Native.GP_ERROR_IO_WRITE, "GP_ERROR_IO_WRITE");
+        m.put(GPhoto2Native.GP_ERROR_IO_UPDATE, "GP_ERROR_IO_UPDATE");
+        m.put(GPhoto2Native.GP_ERROR_IO_SERIAL_SPEED, "GP_ERROR_IO_SERIAL_SPEED");
+        m.put(GPhoto2Native.GP_ERROR_IO_USB_CLEAR_HALT, "GP_ERROR_IO_USB_CLEAR_HALT");
+        m.put(GPhoto2Native.GP_ERROR_IO_USB_FIND, "GP_ERROR_IO_USB_FIND");
+        m.put(GPhoto2Native.GP_ERROR_IO_USB_CLAIM, "GP_ERROR_IO_USB_CLAIM");
+        m.put(GPhoto2Native.GP_ERROR_IO_LOCK, "GP_ERROR_IO_LOCK");
+        m.put(GPhoto2Native.GP_ERROR_HAL, "GP_ERROR_HAL");
     }
 }
--- src/main/java/org/gphoto2/jna/GPhoto2Native.java
+++ src/main/java/org/gphoto2/jna/GPhoto2Native.java
@@ -1,20 +1,20 @@
 /**
- * Java bindings for the libgphoto2 library.
- * Copyright (C) 2011 Innovatrics s.r.o.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Java bindings for the libgphoto2 library. Copyright (C) 2011 Innovatrics
+ * s.r.o.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 package org.gphoto2.jna;
 
@@ -29,6 +29,7 @@ import com.sun.jna.ptr.PointerByReference;
 
 /**
  * Native binding for GPhoto2.
+ *
  * @author Martin Vysny
  */
 public interface GPhoto2Native extends Library {
@@ -40,81 +41,180 @@ public interface GPhoto2Native extends Library {
      * data has been received that can not be automatically handled. Normally,
      * drivers will do everything possible to automatically recover from this
      * error.
-     **/
+     *
+     */
     public static final int GP_ERROR_CORRUPTED_DATA = -102;
     /**
      * File already exists
      *
      * An operation failed because a file existed. This error is reported for
      * example when the user tries to create a file that already exists.
-     **/
+     *
+     */
     public static final int GP_ERROR_FILE_EXISTS = -103;
     /**
      * Specified camera model was not found
      *
-     * The specified model could not be found. This error is reported when
-     * the user specified a model that does not seem to be supported by
-     * any driver.
-     **/
+     * The specified model could not be found. This error is reported when the
+     * user specified a model that does not seem to be supported by any driver.
+     *
+     */
     public static final int GP_ERROR_MODEL_NOT_FOUND = -105;
     /**
      * Specified directory was not found
      *
      * The specified directory could not be found. This error is reported when
      * the user specified a directory that is non-existent.
-     **/
+     *
+     */
     public static final int GP_ERROR_DIRECTORY_NOT_FOUND = -107;
     /**
      * Specified file was not found
      *
-     * The specified file could not be found. This error is reported when
-     * the user wants to access a file that is non-existent.
-     **/
+     * The specified file could not be found. This error is reported when the
+     * user wants to access a file that is non-existent.
+     *
+     */
     public static final int GP_ERROR_FILE_NOT_FOUND = -108;
     /**
      * Specified directory already exists
      *
-     * The specified directory already exists. This error is reported for example
-     * when the user wants to create a directory that already exists.
-     **/
+     * The specified directory already exists. This error is reported for
+     * example when the user wants to create a directory that already exists.
+     *
+     */
     public static final int GP_ERROR_DIRECTORY_EXISTS = -109;
     /**
      * The camera is already busy
      *
      * Camera I/O or a command is in progress.
-     **/
+     *
+     */
     public static final int GP_ERROR_CAMERA_BUSY = -110;
     /**
      * Path is not absolute
      *
      * The specified path is not absolute. This error is reported when the user
-     * specifies paths that are not absolute, i.e. paths like "path/to/directory".
-     * As a rule of thumb, in gphoto2, there is nothing like relative paths.
-     **/
+     * specifies paths that are not absolute, i.e. paths like
+     * "path/to/directory". As a rule of thumb, in gphoto2, there is nothing
+     * like relative paths.
+     *
+     */
     public static final int GP_ERROR_PATH_NOT_ABSOLUTE = -111;
     /**
      * Cancellation successful.
      *
      * A cancellation requestion by the frontend via progress callback and
-     * GP_CONTEXT_FEEDBACK_CANCEL was successful and the transfer has been aborted.
+     * GP_CONTEXT_FEEDBACK_CANCEL was successful and the transfer has been
+     * aborted.
      */
     public static final int GP_ERROR_CANCEL = -112;
     /**
      * Unspecified camera error
      *
-     * The camera reported some kind of error. This can be either a
-     * photographic error, such as failure to autofocus, underexposure, or
-     * violating storage permission, anything else that stops the camera
-     * from performing the operation.
+     * The camera reported some kind of error. This can be either a photographic
+     * error, such as failure to autofocus, underexposure, or violating storage
+     * permission, anything else that stops the camera from performing the
+     * operation.
      */
     public static final int GP_ERROR_CAMERA_ERROR = -113;
     /**
      * Unspecified failure of the operating system
      *
-     * There was some sort of OS error in communicating with the camera,
-     * e.g. lack of permission for an operation.
+     * There was some sort of OS error in communicating with the camera, e.g.
+     * lack of permission for an operation.
      */
     public static final int GP_ERROR_OS_FAILURE = -114;
+    /**
+     * \brief Everything is OK
+     *
+     * Note that this is also the value 0, and every error is negative (lower).
+     */
+    public static final int GP_OK = 0;
+    /**
+     * \brief Generic Error
+     */
+    public static final int GP_ERROR = -1;
+    /**
+     * \brief Bad parameters passed
+     */
+    public static final int GP_ERROR_BAD_PARAMETERS = -2;
+    /**
+     * \brief Out of memory
+     */
+    public static final int GP_ERROR_NO_MEMORY = -3;
+    /**
+     * \brief Error in the camera driver
+     */
+    public static final int GP_ERROR_LIBRARY = -4;
+    /**
+     * \brief Unknown libgphoto2 port passed
+     */
+    public static final int GP_ERROR_UNKNOWN_PORT = -5;
+    /**
+     * \brief Functionality not supported
+     */
+    public static final int GP_ERROR_NOT_SUPPORTED = -6;
+    /**
+     * \brief Generic I/O error
+     */
+    public static final int GP_ERROR_IO = -7;
+    /**
+     * \brief Buffer overflow of internal structure
+     */
+    public static final int GP_ERROR_FIXED_LIMIT_EXCEEDED = -8;
+    /**
+     * \brief Operation timed out
+     */
+    public static final int GP_ERROR_TIMEOUT = -10;
+    /**
+     * \brief Serial ports not supported
+     */
+    public static final int GP_ERROR_IO_SUPPORTED_SERIAL = -20;
+    /**
+     * \brief USB ports not supported
+     */
+    public static final int GP_ERROR_IO_SUPPORTED_USB = -21;
+    /**
+     * \brief Error initialising I/O
+     */
+    public static final int GP_ERROR_IO_INIT = -31;
+    /**
+     * \brief I/O during read
+     */
+    public static final int GP_ERROR_IO_READ = -34;
+    /**
+     * \brief I/O during write
+     */
+    public static final int GP_ERROR_IO_WRITE = -35;
+    /**
+     * \brief I/O during update of settings
+     */
+    public static final int GP_ERROR_IO_UPDATE = -37;
+    /**
+     * \brief Specified serial speed not possible.
+     */
+    public static final int GP_ERROR_IO_SERIAL_SPEED = -41;
+    /**
+     * \brief Error during USB Clear HALT
+     */
+    public static final int GP_ERROR_IO_USB_CLEAR_HALT = -51;
+    /**
+     * \brief Error when trying to find USB device
+     */
+    public static final int GP_ERROR_IO_USB_FIND = -52;
+    /**
+     * \brief Error when trying to claim the USB device
+     */
+    public static final int GP_ERROR_IO_USB_CLAIM = -53;
+    /**
+     * \brief Error when trying to lock the device
+     */
+    public static final int GP_ERROR_IO_LOCK = -60;
+    /**
+     * \brief Unspecified error when talking to HAL
+     */
+    public static final int GP_ERROR_HAL = -70;
     public static final int GP_CAPTURE_IMAGE = 0;
     public static final int GP_CAPTURE_MOVIE = 1;
     public static final int GP_CAPTURE_SOUND = 2;
@@ -152,7 +252,7 @@ public interface GPhoto2Native extends Library {
     int gp_camera_unref(Pointer camera);
 
     int gp_camera_get_config(Pointer camera, PointerByReference widget, Pointer context);
-    
+
     int gp_camera_set_config(Pointer camera, Pointer widget, Pointer context);
 
     int gp_file_ref(Pointer cf);
@@ -188,33 +288,32 @@ public interface GPhoto2Native extends Library {
     void gp_abilities_list_free(Pointer cameraAbilitiesList);
 
     void gp_port_info_list_free(Pointer portInfoList);
-    
+
     int gp_port_info_list_get_info(Pointer portInfoList, int n, PointerByReference portInfo);
-    
+
     int gp_camera_set_port_info(Pointer camera, Pointer portInfo);
 
     /**
      * A structure created by the capture operation.
      *
-     * A structure containing the folder and filename of an object
-     * after a successful capture and is passed as reference to the
-     * gp_camera_capture() function.
+     * A structure containing the folder and filename of an object after a
+     * successful capture and is passed as reference to the gp_camera_capture()
+     * function.
      */
     public static class CameraFilePath extends Structure {
 
-	/**
-	 * Name of the captured file.
-	 */
-	public byte[] name = new byte[128];
-	/**
-	 * Name of the folder of the captured file.
-	 */
-	public byte[] folder = new byte[1024];
+        /**
+         * Name of the captured file.
+         */
+        public byte[] name = new byte[128];
+        /**
+         * Name of the folder of the captured file.
+         */
+        public byte[] folder = new byte[1024];
 
-	public static class ByReference extends CameraFilePath implements Structure.ByReference {
-	};
+        public static class ByReference extends CameraFilePath implements Structure.ByReference {
+        };
     }
-
     public static final int GP_WIDGET_WINDOW = 0;//  # Window widget This is the toplevel configuration widget. It should likely contain multiple GP_WIDGET_SECTION entries.
     public static final int GP_WIDGET_SECTION = 1;// # Section widget (think Tab).
     public static final int GP_WIDGET_TEXT = 2;//    # Text widget.
@@ -262,13 +361,19 @@ public interface GPhoto2Native extends Library {
 
     int gp_widget_set_info(Pointer widget, String info);
 
-    int gp_widget_get_info(Pointer widget, PointerByReference /**char ** **/ info);
+    int gp_widget_get_info(Pointer widget, PointerByReference /**
+                     * char ** *
+                     */
+                    info);
 
     int gp_widget_get_id(Pointer widget, IntByReference id);
 
     int gp_widget_get_type(Pointer widget, IntByReference type);
 
-    int gp_widget_get_label(Pointer widget, PointerByReference /**char ** **/ label);
+    int gp_widget_get_label(Pointer widget, PointerByReference /**
+                     * char ** *
+                     */
+                    label);
 
     int gp_widget_set_range(Pointer range, float low, float high, float increment);
 
@@ -278,7 +383,10 @@ public interface GPhoto2Native extends Library {
 
     int gp_widget_count_choices(Pointer widget);
 
-    int gp_widget_get_choice(Pointer widget, int choice_number, PointerByReference /**char ** **/ choice);
+    int gp_widget_get_choice(Pointer widget, int choice_number, PointerByReference /**
+                     * char ** *
+                     */
+                    choice);
 
     int gp_widget_changed(Pointer widget);
 
@@ -288,4 +396,3 @@ public interface GPhoto2Native extends Library {
 
     int gp_widget_get_readonly(Pointer widget, IntByReference readonly);
 }
-
